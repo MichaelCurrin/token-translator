921c60e66a3d93db318ea0b4975ddb90a2cfb8da
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-05-28 14:15:50 +0200

    style: update constants.js and lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,39 +1,38 @@
-import { WORDS_PER_A4_PAGE, WORDS_PER_A5_BOOK, WORDS_PER_TOKEN } from './constants';
-
-
+import {
+  WORDS_PER_A4_PAGE,
+  WORDS_PER_A5_BOOK,
+  WORDS_PER_TOKEN,
+} from "./constants";
 
 function roundToOneDecimal(num) {
-    return Math.round(num * 10) / 10;
+  return Math.round(num * 10) / 10;
 }
-
 
 export function tokenCountToWordCount(tokenCount) {
-    return tokenCount * WORDS_PER_TOKEN;
+  return tokenCount * WORDS_PER_TOKEN;
 }
 
-
 export function wordCountToPageCounts(wordCount) {
-    const a4PageCount = wordCount / WORDS_PER_A4_PAGE;
-    const a5NovelCount = wordCount / WORDS_PER_A5_BOOK;
+  const a4PageCount = wordCount / WORDS_PER_A4_PAGE;
+  const a5NovelCount = wordCount / WORDS_PER_A5_BOOK;
 
-    return {
-        a4PageCount: a4PageCount,
-        a5NovelCount: a5NovelCount
-    };
+  return {
+    a4PageCount: a4PageCount,
+    a5NovelCount: a5NovelCount,
+  };
 }
 
 export function addCounts(model) {
-    const wordCount = tokenCountToWordCount(model.tokens);
-    const { a4PageCount, a5NovelCount } = wordCountToPageCounts(wordCount);
+  const wordCount = tokenCountToWordCount(model.tokens);
+  const { a4PageCount, a5NovelCount } = wordCountToPageCounts(wordCount);
 
-    return {
-        ...model,
-        wordCount: roundToOneDecimal(wordCount),
-        a4PageCount: roundToOneDecimal(a4PageCount),
-        a5NovelCount: roundToOneDecimal(a5NovelCount)
-    };
+  return {
+    ...model,
+    wordCount: roundToOneDecimal(wordCount),
+    a4PageCount: roundToOneDecimal(a4PageCount),
+    a5NovelCount: roundToOneDecimal(a5NovelCount),
+  };
 }
-
 
 // // User input.
 // const tokenCount = 10001;
