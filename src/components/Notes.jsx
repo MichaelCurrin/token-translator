b149a5e0a6b07c685a2ed21553b6941c29d1dac4
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-05-29 21:31:03 +0200

    feat: create Notes.jsx

---
+++ src/components/Notes.jsx
@@ -0,0 +1,11 @@
+function Notes() {
+    return (
+        <div>
+            The given text is not broken into actual tokens but just using a rough estimate of 100 words = 75 tokens.
+            Models limits are sometimes given as a "context window" and sometimes as "input" and "output" tokens. Whenever the second approach is used, only the "input" value is used.
+
+        </div>
+    )
+}
+
+export default Notes
