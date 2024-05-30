471baa67ca554ead0c593775529d25fd52216503
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-05-30 14:54:53 +0200

    feat: add image header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,21 @@ function Header() {
     <header id="header">
       <h1>Token Translator</h1>
 
-      <a
-        href="https://github.com/MichaelCurrin/token-translator"
-        title="Go to GitHub repo"
-      >
-        <img
-          src="https://img.shields.io/static/v1?label=MichaelCurrin&message=token-translator&color=blue&logo=github"
-          alt="MichaelCurrin - token-translator"
-        />
-      </a>
+      <p><i>Compare the input token limits of GPT models as real-world measures</i></p>
+      <img src="token-translator/robot-art.jpg" alt="Robot holding books" width="202"></img>
+
+      <div>
+        <a
+          href="https://github.com/MichaelCurrin/token-translator"
+          title="Go to GitHub repo"
+        >
+          <img
+            src="https://img.shields.io/static/v1?label=MichaelCurrin&message=token-translator&color=blue&logo=github"
+            alt="MichaelCurrin - token-translator"
+          />
+        </a></div>
+
+
     </header>
   );
 }
