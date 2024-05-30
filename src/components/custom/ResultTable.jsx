0d33be4d4f4170d4e3ac1feab21f2175d08a9650
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-05-30 15:00:34 +0200

    style: update InputForm.jsx and ResultTable.jsx

--- src/components/custom/ResultTable.jsx
+++ src/components/custom/ResultTable.jsx
@@ -1,43 +1,43 @@
 import React from "react";
 
 function ResultTable({ result }) {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Tokens</th>
-                    <th>Words</th>
-                    <th>A4 pages</th>
-                    <th>Novels</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td className="numeric" data-testid="user-table-tokens">
-                        {result.tokens.toLocaleString()}
-                    </td>
-                    <td className="numeric" data-testid="user-table-word-count">
-                        {result.wordCount.toLocaleString(undefined, {
-                            minimumFractionDigits: 0,
-                            maximumFractionDigits: 0,
-                        })}
-                    </td>
-                    <td className="numeric" data-testid="user-table-page-count">
-                        {result.a4PageCount.toLocaleString(undefined, {
-                            minimumFractionDigits: 1,
-                            maximumFractionDigits: 1,
-                        })}
-                    </td>
-                    <td className="numeric" data-testid="user-table-novel-count">
-                        {result.a5NovelCount.toLocaleString(undefined, {
-                            minimumFractionDigits: 1,
-                            maximumFractionDigits: 1,
-                        })}
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    );
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Tokens</th>
+          <th>Words</th>
+          <th>A4 pages</th>
+          <th>Novels</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="numeric" data-testid="user-table-tokens">
+            {result.tokens.toLocaleString()}
+          </td>
+          <td className="numeric" data-testid="user-table-word-count">
+            {result.wordCount.toLocaleString(undefined, {
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
+          </td>
+          <td className="numeric" data-testid="user-table-page-count">
+            {result.a4PageCount.toLocaleString(undefined, {
+              minimumFractionDigits: 1,
+              maximumFractionDigits: 1,
+            })}
+          </td>
+          <td className="numeric" data-testid="user-table-novel-count">
+            {result.a5NovelCount.toLocaleString(undefined, {
+              minimumFractionDigits: 1,
+              maximumFractionDigits: 1,
+            })}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 export default ResultTable
