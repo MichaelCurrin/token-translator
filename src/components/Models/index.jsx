5e2b326476e8a1ced8c29fd10b00b41dd53b72a8
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-06-01 19:47:53 +0200

    refactor: update src/components/Models/index.jsx

--- src/components/Models/index.jsx
+++ src/components/Models/index.jsx
@@ -4,35 +4,39 @@ import { addCounts } from '../../lib';
 import SortingButtons from './SortingButtons';
 import Table from './Table';
 
+function sortModels(key, models) {
+  let sortedArray = [...models];
+
+  if (key === 'providerAndModel') {
+    sortedArray.sort((a, b) => {
+      if (a.provider === b.provider) {
+        return a.modelName.localeCompare(b.modelName);
+      }
+      return a.provider.localeCompare(b.provider);
+    });
+  } else if (key === 'tokens') {
+    sortedArray.sort((a, b) => b.tokens - a.tokens);
+  }
+
+  return sortedArray;
+}
+
 function Models() {
   const [sortedModels, setSortedModels] = useState(
     MODEL_INPUT_LIMITS.map(addCounts),
   );
-  const [sortBy, setSoryBy] = useState('providerAndModel');
+  const [sortBy, setSortBy] = useState('providerAndModel');
 
-  const sortModels = (key) => {
-    let sortedArray = [...sortedModels];
-
-    if (key === 'providerAndModel') {
-      sortedArray.sort((a, b) => {
-        if (a.provider === b.provider) {
-          return a.modelName.localeCompare(b.modelName);
-        }
-        return a.provider.localeCompare(b.provider);
-      });
-    } else if (key === 'tokens') {
-      sortedArray.sort((a, b) => b.tokens - a.tokens);
-    }
-
-    setSortedModels(sortedArray);
-    setSoryBy(key);
+  const onSort = (key) => {
+    setSortedModels(sortModels(key, sortedModels));
+    setSortBy(key);
   };
 
   return (
     <div>
       <SortingButtons
         sortBy={sortBy}
-        onSort={sortModels}
+        onSort={onSort}
         className="sorting-buttons"
       />
       <Table models={sortedModels} />
