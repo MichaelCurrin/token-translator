958e935f3bb0dedce000a292f1aee0425ae4d04b
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-06-01 19:24:19 +0200

    update Table.jsx and src/components/Pricing/index.jsx

--- src/components/Pricing/index.jsx
+++ src/components/Pricing/index.jsx
@@ -2,12 +2,15 @@ import { PRICE_PER_MILLION_TOKENS } from '../../constants';
 import Table from './Table';
 
 function Pricing() {
-    return (
-        <div>
-            <p>Prices for using the model through an API, as price per 1 million tokens (as given by the sources)</p>
-            <Table models={PRICE_PER_MILLION_TOKENS} />
-        </div>
-    );
+  return (
+    <div>
+      <p>
+        Prices for using the model through an API, as price per 1 million tokens
+        (as given by the sources under <a href="#api-pricing">API Pricing</a>)
+      </p>
+      <Table models={PRICE_PER_MILLION_TOKENS} />
+    </div>
+  );
 }
 
 export default Pricing;
