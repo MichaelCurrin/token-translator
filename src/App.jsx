0f6b243808481c0b6acfc628841a45728521b274
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-06-10 13:30:44 +0200

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,24 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <h2>About</h2>
+      <p>
+        Language models like GPT-4 or LLaMA can process a lot of text, but the
+        amount of text they can handle is not always easy to visualize.
+        Especially since the limit of a model is quoted not in characters or
+        words in "tokens", which are groups of characters.{' '}
+      </p>
+      <p>
+        This tool helps you translate token counts into real-world measures so
+        you can decide which models is suitable for your needs and how much
+        you're likelly to pay if using an API subscription.
+      </p>
+      <blockquote>
+        <p>
+          ℹ️ This tool is open sourced on GitHub, if you'd like to suggest or
+          contribute changes
+        </p>
+      </blockquote>
       <div>
         <h2>Popular models</h2>
         <Models />
