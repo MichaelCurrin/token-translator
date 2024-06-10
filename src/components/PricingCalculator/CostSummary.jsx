367302de01be0cfc29e8f7d8a9c5e583c7ee347c
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-06-10 20:24:02 +0200

    feat: convert to table

--- src/components/PricingCalculator/CostSummary.jsx
+++ src/components/PricingCalculator/CostSummary.jsx
@@ -1,34 +1,46 @@
 export function CostSummary(props) {
   return (
-    <>
-      <p>Total input tokens: {props.totalInputTokens.toLocaleString()}</p>
-      <p>Total output tokens: {props.totalOutputTokens.toLocaleString()}</p>
-      <p>Combined tokens: {props.totalTokens.toLocaleString()}</p>
-
-      <p>
-        Total input cost: $
-        {props.totalInputCost.toLocaleString(undefined, {
-          minimumFractionDigits: 6,
-          maximumFractionDigits: 6,
-        })}
-      </p>
-      <p>
-        Total output cost: $
-        {props.totalOutputCost.toLocaleString(undefined, {
-          minimumFractionDigits: 6,
-          maximumFractionDigits: 6,
-        })}
-      </p>
-      <p>
-        Combined cost (rounded):{' '}
-        <b>
-          $
-          {props.totalCost.toLocaleString(undefined, {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-          })}
-        </b>
-      </p>
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Input</th>
+          <th>Output</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Tokens</td>
+          <td className="align-right">{props.totalInputTokens.toLocaleString()}</td>
+          <td className="align-right">{props.totalOutputTokens.toLocaleString()}</td>
+          <td className="align-right">{props.totalTokens.toLocaleString()}</td>
+        </tr>
+        <tr>
+          <td>Costs</td>
+          <td className="align-right">
+            $
+            {props.totalInputCost.toLocaleString(undefined, {
+              minimumFractionDigits: 4,
+              maximumFractionDigits: 6,
+            })}
+          </td>
+          <td className="align-right">
+            $
+            {props.totalOutputCost.toLocaleString(undefined, {
+              minimumFractionDigits: 4,
+              maximumFractionDigits: 6,
+            })}
+          </td>
+          <td className="align-right">
+            <b>$
+              {props.totalCost.toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}</b>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
