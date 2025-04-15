6be890918993cfd83b7beb2ac72fc1fac3d027b5
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2025-04-15 14:15:46 +0200

    update constants.js

--- src/constants.js
+++ src/constants.js
@@ -30,6 +30,7 @@ export const MODEL_INPUT_LIMITS = [
   { provider: 'OpenAI', modelName: 'GPT-4o', tokens: 128000 },
   { provider: 'OpenAI', modelName: 'GPT-4 Turbo', tokens: 128000 },
   { provider: 'OpenAI', modelName: 'GPT-3.5 Turbo', tokens: 16385 },
+  { provider: 'Meta', modelName: 'Llama 4 Scout', tokens: 10 * 10 ** 6 },
   { provider: 'Meta', modelName: 'Llama 3', tokens: 16000 },
   { provider: 'Meta', modelName: 'Llama 2', tokens: 8000 },
   { provider: 'Meta', modelName: 'Phi-3', tokens: 128000 },
@@ -38,7 +39,7 @@ export const MODEL_INPUT_LIMITS = [
   { provider: 'Anthropic', modelName: 'Claude 3.5', tokens: 200000 },
   { provider: 'Anthropic', modelName: 'Claude 3.0', tokens: 200000 },
   { provider: 'Anthropic', modelName: 'Claude 2.1', tokens: 200000 },
-  { provider: 'MiniMaxAI', modelName: 'MiniMax-Text-01', tokens: 4000000 },
+  { provider: 'MiniMaxAI', modelName: 'MiniMax-Text-01', tokens: 4 * 10 ** 6 },
 ];
 
 // Assume price is in Dollars. Better to keep as string not number, as readable
